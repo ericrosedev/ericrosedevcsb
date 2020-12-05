ff47a4518a940d18041d1b1dd0ed175787701810
Eric Rose <75504635+ericrosedev@users.noreply.github.com>
2020-12-05 15:42:42 -0800

    image testing testing image sourcing for pngs and svgs

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <template>
   <div id="app">
-    <img alt="png test" src="./assets/svg/test.png" width="25%" />
-    <img alt="svg test" src="test.svg" width="25%" />
-    <TestSVG />
+    <!-- <img alt="png test" src="./assets/svg/test.png" width="25%" /> -->
     <HelloWorld msg="Hello Vue in CodeSandbox!" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld";
-import TestSVG from "./assets/png/test.svg";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
-    TestSVG,
   },
 };
 </script>
@@ -28,7 +24,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  background: lightblue url("test.svg") no-repeat left;
+  /* background: url("test.svg") no-repeat left; */
   background-size: 200px 200px;
 }
 </style>
---
+++ src/components/svg/InlineSvg.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    version="1.1"
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 63.9 40.3"
+    enable-background="new 0 0 63.9 40.3"
+    xml:space="preserve"
+  >
+    <text
+      transform="matrix(1 0 0 1 1.171875e-02 10.4164)"
+      font-family="'MyriadPro-Semibold'"
+      font-size="12px"
+    >
+      Test
+    </text>
+    <circle fill="#6ABD45" cx="43.8" cy="20.1" r="20.1" />
+  </svg>
+</template>
